bbc31888b716ebb86e0aef4a4f17e3fa247b78dd
Sergio Gil <sgilperez@gmail.com>
2022-10-31 08:20:49 +0100

    Refactor the object code

---
+++ sonic-pi-connection.el
@@ -0,0 +1,52 @@
+(require 'osc)
+(require 'eieio)
+
+(defclass sonic-pi--connection ()
+  ((daemon :type process)
+   (api-client :type process)
+   (keep-alive-client :type process)
+   (keep-alive-timer :type timer)
+   (log-server :type process)
+   (token :type number)
+   (connected? :initform nil :type boolean)))
+
+(cl-defmethod sonic-pi--connection--connect ((c sonic-pi--connection))
+  (sonic-pi-messages-buffer-init)
+  (message "Starting Sonic Pi daemon...")
+  (with-slots (daemon api-client keep-alive-client keep-alive-timer log-server token connected?) c
+    (when (not connected?)
+      (setf daemon (start-process-shell-command "sonic-pi-daemon" "*sonic-pi-daemon-output*" sonic-pi-daemon-path))
+      (set-process-filter daemon (lambda (_ output)
+                                   (cl-destructuring-bind (reported-daemon-keep-alive reported-gui-listen-to-server reported-gui-send-to-server reported-scsynth reported-osc-cues reported-tau-api reported-tau-phx reported-token)
+                                       (seq-map #'string-to-number (split-string output " "))
+                                     (setf token reported-token)
+                                     (setf keep-alive-client (osc-make-client 'local reported-daemon-keep-alive))
+                                     (setf keep-alive-timer (run-with-timer 30 30 (lambda ()
+                                                                                    (osc-send-message keep-alive-client "/daemon/keep-alive" token))))
+                                     (setf api-client (osc-make-client 'local reported-gui-send-to-server))
+                                     (setf log-server (osc-make-server 'local reported-gui-listen-to-server (lambda (path &rest args) (sonic-pi-log-message path args))))
+                                     (setf connected? t))
+                                   (set-process-filter daemon nil))) ; don't need to get more lines
+      (while (not connected?) (accept-process-output daemon))) ; wait until we connect
+    (message "Sonic Pi connected")))
+
+;; TODO: Bring this back for all processes
+;; (set-process-query-on-exit-flag (oref c :api-client) nil)) ; let emacs kill it on exit without confirmation
+
+(cl-defmethod sonic-pi--connection--send ((c sonic-pi--connection) command &rest args)
+  (with-slots (connected? api-client token) c
+    (if connected?
+        (apply #'osc-send-message api-client command token args)
+      (message "Not connected! Connect to Sonic Pi first"))))
+
+(cl-defmethod sonic-pi--connection--disconnect ((c sonic-pi--connection))
+  (with-slots (keep-alive-timer api-client keep-alive-client log-server daemon connected?) c
+    (when connected?
+      (cancel-timer keep-alive-timer)
+      (delete-process api-client)
+      (delete-process keep-alive-client)
+      (delete-process log-server)
+      (delete-process daemon)
+      (sonic-pi-messages-buffer-cleanup)
+      (setf connected? nil)))
+  (message "Sonic Pi disconnected"))
--- sonic-pi-mode.el
+++ sonic-pi-mode.el
@@ -28,9 +28,8 @@
 ;; See documentation at https://github.com/porras/sonic-pi-mode
 
 ;;; Code:
-(require 'osc)
-(require 'eieio)
-; taken from sonic-pi.el
+(require 'sonic-pi-connection)
+;; taken from sonic-pi.el
 (require 'sonic-pi-console)
 
 (defvar sonic-pi-mode-map
@@ -43,7 +42,7 @@
 
 (defvar sonic-pi-daemon-path "flatpak run --command=\"/app/app/server/ruby/bin/daemon.rb\" net.sonic_pi.SonicPi")
 ;; (defvar sonic-pi-daemon-path "~/Code/sonic-pi/app/server/ruby/bin/daemon.rb")
-(defvar sonic-pi-connection nil)
+(defvar sonic-pi-connection (make-instance sonic-pi--connection))
 
 ;;;###autoload
 (define-minor-mode sonic-pi-mode
@@ -56,81 +55,24 @@
 
 (defun sonic-pi-connect ()
   (interactive)
-  (if sonic-pi-connection
-      (message "Already connected")
-    (setq sonic-pi-connection (sonic-pi--make-connection))))
+  (sonic-pi--connection--connect sonic-pi-connection))
 
 (defun sonic-pi-disconnect ()
   (interactive)
-  (if sonic-pi-connection
-      (progn (sonic-pi--connection--close sonic-pi-connection)
-             (setq sonic-pi-connection nil)
-             (message "Disconnected"))
-    (message "Not connected")))
+  (sonic-pi--connection--disconnect sonic-pi-connection))
 
 (defun sonic-pi-send-buffer ()
   (interactive)
-  (if sonic-pi-connection
-      (progn
-        (sonic-pi--flash-mode-line)
-        (sonic-pi--connection--send sonic-pi-connection "/run-code" (buffer-string))
-        (message (format "Sent %i characters of 🎶" (length (buffer-string)))))
-    (message "Not connected to Sonic Pi. Connect first (sonic-pi-connect or C-c c)")))
+  (sonic-pi--flash-mode-line)
+  (sonic-pi--connection--send sonic-pi-connection "/run-code" (buffer-string)))
 
 (defun sonic-pi-stop ()
   (interactive)
-  (if sonic-pi-connection
-      (progn
-        (sonic-pi--connection--send sonic-pi-connection "/stop-all-jobs")
-        (message "Stop"))
-    (message "Not connected to Sonic Pi. Connect first (sonic-pi-connect or C-c c)")))
+  (sonic-pi--connection--send sonic-pi-connection "/stop-all-jobs"))
 
 (defun sonic-pi--flash-mode-line ()
   "Taken from https://www.emacswiki.org/emacs/AlarmBell#h5o-3"
   (invert-face 'mode-line)
   (run-with-timer 0.2 nil #'invert-face 'mode-line))
-
-(defclass sonic-pi--connection ()
-  ((daemon :initarg :daemon :type process)
-   (api-client :initarg :api-client :type process)
-   (keep-alive-client :initarg :keep-alive-client :type process)
-   (keep-alive-timer :initarg :keep-alive-timer :type timer)
-   (log-server :initarg :log-server :type process)
-   (token :initarg :token :type number)))
-
-;; TODO: This function is a mess, almost everything happens here! Refactor
-(defun sonic-pi--make-connection ()
-  (sonic-pi-messages-buffer-init)
-  (message "Starting Sonic Pi daemon...")
-  (let ((c (sonic-pi--connection))
-        (daemon (start-process-shell-command "sonic-pi-daemon" "sonic-pi-daemon-output" sonic-pi-daemon-path)))
-    (set-process-filter daemon (lambda (_ output)
-                                 (cl-destructuring-bind (daemon-keep-alive gui-listen-to-server gui-send-to-server scsynth osc-cues tau-api tau-phx token) ; we don't need them all but better name them
-                                     (seq-map #'string-to-number (split-string output " "))
-                                   (oset c :token token)
-                                   (oset c :keep-alive-client (osc-make-client 'local daemon-keep-alive))
-                                   (oset c :keep-alive-timer (run-with-timer 30 30 (lambda ()
-                                                                                     (osc-send-message (oref c :keep-alive-client) "/daemon/keep-alive" token))))
-                                   (oset c :api-client (osc-make-client 'local gui-send-to-server))
-                                   (oset c :log-server (osc-make-server 'local gui-listen-to-server (lambda (path &rest args) (sonic-pi-log-message path args)))))
-                                 (set-process-filter daemon nil)
-                                 (oset c :daemon daemon)))
-    (while (not (slot-boundp c :daemon)) (accept-process-output daemon)) ; wait until we get the output
-    (message "Sonic Pi daemon started")
-    c))
-
-;; TODO: Bring this back for all processes
-;; (set-process-query-on-exit-flag (oref c :api-client) nil)) ; let emacs kill it on exit without confirmation
-
-(cl-defmethod sonic-pi--connection--send ((c sonic-pi--connection) command &rest args)
-  (apply #'osc-send-message (oref c :api-client) command (oref c :token) args))
-
-(cl-defmethod sonic-pi--connection--close ((c sonic-pi--connection))
-  (cancel-timer (oref c :keep-alive-timer))
-  (delete-process (oref c :api-client))
-  (delete-process (oref c :keep-alive-client))
-  (delete-process (oref c :log-server))
-  (delete-process (oref c :daemon))
-  (sonic-pi-messages-buffer-cleanup))
 
 (provide 'sonic-pi-mode)
